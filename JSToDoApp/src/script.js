1105892ee8bd74d8d04826fb415ac51745d89854
shanthikaruna <shanthi.karuna01@gmail.com>
2023-02-23 09:33:13 -0800

    update functionaliity added

--- JSToDoApp/src/script.js
+++ JSToDoApp/src/script.js
@@ -1,12 +1,7 @@
 
-
-
 let today =  new Date()
 let date = today.toDateString().split(' ')
 let time = today.toLocaleTimeString()
-
-console.log(today.toDateString())
-
 let dateElement = document.getElementById('date_text');
 let timeElement = document.getElementById('time_text');
 
@@ -16,24 +11,17 @@ timeElement.textContent = time
 const apiURL = 'https://uptight-teal-walrus.cyclic.app/task'
 
 
-
-
-const  fetchPromise = fetch(apiURL)
-//console.log("fetchPromise", fetchPromise)
+/*const  fetchPromise = fetch(apiURL)
 fetchPromise.then((response) => {
   const jsonResponse = response.json()
   jsonResponse.then((data) => {
     console.log(data[0])
   })
-  
-})
-
+})*/
 
 
 const createTask = (task) => {
   let taskJSON = JSON.stringify(task)
-  //console.log("taskJSON", taskJSON)
-
   const requestOptions = {
     method: "POST",
     headers: {
@@ -43,8 +31,8 @@ const createTask = (task) => {
   }
   fetch(apiURL, requestOptions)
   .then(res => res)
-  //.then(res => res.json()) //the .json method parses the JSON response into a JS Object literal
-  .then(data => getTask())
+  //.then(res => res.json()) //the .json method parses the JSON response into a JS Object literal. causes an error as reesponse from api returned is text/html
+  .then(data => getTask()) //when the Promise is resolved, create the task dynamically
   .catch(err => console.error(err))
 }
 
@@ -60,154 +48,111 @@ task_form.addEventListener('submit', (event) => {
   }
   createTask(newTask)
   event.target.task.value = ''
- 
 })
 
 
 
+const callUpdateAPI  = (isChecked,dataID) => {
+    console.log("insidee callUpdateAPI isChecked", isChecked)
+    console.log("insidee callUpdateAPI dataID", dataID)
 
+    let newTask = {
+      isCompleted: isChecked
+    }
+
+    let taskJSON = JSON.stringify(newTask)
+
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: taskJSON
+    }
+    fetch(`${apiURL}/${dataID}`, requestOptions)
+    .then(res => res)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  
+}
+
+const checkboxClick = (e) => {
+  let isChecked = e.target.checked
+  console.log("this.checked",  e.target.checked)
+  console.log("this.dataid",  e.target.dataID)
+
+ // callUpdateAPI(isChecked,e.target.dataID)
+}
 
 //get a task
-
 const getTask = () => {
- 
+  console.log("get task loaded")
   fetch(apiURL)
   .then(res => res.json())
   .then(res => {
-      //console.log("data", res)
-
+      console.log("getTask data", res)
       let task_container = document.getElementById('task_container')
-
       res.map((item)=>{
         let task_li = document.createElement("li")
         //task_li.id = 'task_div'
         task_li.id = `${item._id}`
         task_li.classList.add("todo", "stack-small")
         task_li.innerHTML = `<div class="todo_labels"><p>${item.name}</p><p>${item.date}</p></div> <div class="btn_group">
-        <input type="checkbox" name=${item.name} isCompleted=${item.isCompleted}
-        dataId=${item._id} id="taskCheckBox" date=${item.date} class="cbox">
+        <input type="checkbox" name=${item.name} ${item.isCompleted==true?'checked':''} isCompleted=${item.isCompleted} dataId=${item._id}  date=${item.date} class="cbox" onchange="(function(e){const itemid = getAttribute('dataId'); console.log(e); callUpdateAPI(e.target.checked,itemid); })(event)" />
         <img src="../assets/Vector.jpg" alt="" id="img_del" onclick='handleDeleteClick("${item._id}")' />
       </div>`
-    
-    
 
-
+    
+     
       task_container.append(task_li)
 
+      
+      /*let checkbox = document.getElementById("taskCheckBox")
+      checkbox.checked = `${item.isCompleted}`*/
+     
+      
+      /*input.addEventListener('change', (e) =>{
     
-        //editTask()
-     /*let img = document.getElementById("img_del")
-      img.addEventListener('click',()=>{
-        callDelete(item._id)
+        let isChecked = e.target.checked
+        console.log("this.checked",  e.target.checked)
+        callUpdateAPI(isChecked,dataID)
       })*/
 
+
+     /*let img = document.getElementById("img_del")
+      img.addEventListener('click',()=>{
+        handleDeleteClick(item._id)
+      })*/
       })
+      //editTask()
+     /* let checkbox = document.getElementById("taskCheckBox")
+      let dataID = checkbox.getAttribute('dataID')
+      
+      checkbox.addEventListener('change', (e) =>{
+    
+        let isChecked = e.target.checked
+        console.log("this.checked",  e.target.checked)
+        callUpdateAPI(isChecked,dataID)
+      })*/
   })
   
 }
 
 getTask()
 
-
 const handleDeleteClick = (id) => {
   console.log("id is", id)
   const apiURL = 'https://uptight-teal-walrus.cyclic.app/task'
-  
   const requestOptions = {
       method: 'DELETE'
   }
-
-  
-  console.log("id after parsing is", `${apiURL}/${id}`)
-  //fetch(apiURL/id, requestOptions)
-  fetch(`${apiURL}/${id}`, requestOptions)
-  //fetch("https://uptight-teal-walrus.cyclic.app/task/63f42098bbc1f757e377870e", requestOptions)
-          //.then(res => res.json())
-          //.then(res => res)
-          //.then(data => getTask())
-          .then(data => console.log("delete data", data))
-          .catch(err => console.error(err))   
-
   let task_container = document.getElementById('task_container')
   let task_div = document.getElementById(`${id}`)
-  task_container.removeChild(task_div)
-}
-
-/*const editTask = (id, newData) => {
-  let checkbox = document.getElementById("taskCheckBox")
-  checkbox.addEventListener('change', function(){
-      if(this.checked) {
-        this.checked = !this.checked
-      }
-      console.log("this.checked",  this.checked)
-  })
-}*/
-
-
-
-//when you click add button
-  //add a new task with the input text value
-
-//array to store all the tasks
-/*const todoListItems = [];
-
-function addTask(taskVal) {
-  console.log("am i heree")
-  const taskObj = {
-    taskVal,
-    checked: false,
-    taskId: Date.now()
-  }
-  todoListItems.push(taskObj)
-  let list = displayItems(taskObj)
-}
-
-//checked box
-const taskList = document.querySelector("ul")
-taskList.addEventListener('input', (e)=> {
-  const taskID = e.target.closest("li").id
- 
-  updateTask(taskID)
-})
-
-function updateTask(taskID) {
-  console.log("taskID", taskID)
+  
+  fetch(`${apiURL}/${id}`, requestOptions)
+    .then(data => task_container.removeChild(task_div))
+    .catch(err => console.error(err))   
 }
 
 
-/*function isChecked(checkbox) {
-  checkbox.addEventListener('change', function(){
-    if (this.checked) {
-      this.checked = !this.checked
-    }
-    console.log("checked",this.checked)
-  })
-}
 
-
-function displayItems(taskObj) {
-  const taskList = document.querySelector("ul")
-  const isChecked = taskObj.checked ? 'done': ''
-  const list = document.createElement("li")
-  list.setAttribute("id", taskObj.taskId)
-  list.innerHTML = `<span>${taskObj.taskVal}</span>
-  <input type="checkbox" name="checkbox" value=${taskObj.checked} />
-  <button id="delete" class="item-delete">Delete</button>`
-  taskList.append(list)
-  //return list
- 
-}
-const form = document.querySelector("form");
-form.addEventListener('submit', function(e) {
-  e.preventDefault()
-  //grab the input value
-  const input = document.getElementById("taskVal")
-  let taskVal = document.getElementById("taskVal").value
- 
-  //create the todo object
-  if(taskVal!= '') {
-    addTask(taskVal)
-    input.value = ''
-    input.focus()
-  }
-});*/
